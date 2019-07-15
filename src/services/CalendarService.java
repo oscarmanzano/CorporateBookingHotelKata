6c4c3b4c7d743a8cd4a28d6ce7b6103e4c743ef9
oscar <oscar.manzano@codurance.com>
2019-07-15 12:49:50 +0200

    added maven pom file

--- src/services/CalendarService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public class CalendarService {
-    public boolean isBookingAllowed(Object o, Object o1, String existingRoomType, int existingHotelId) {
-        return false;
-    }
-}
